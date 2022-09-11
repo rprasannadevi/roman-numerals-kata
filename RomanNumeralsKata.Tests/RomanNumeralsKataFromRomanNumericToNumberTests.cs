cc8a5c392a3d32cf1728545bbb95d0637a58368c
Prasanna <prasannadevi@gmail.com>
2022-09-11 19:23:32 +0100

    Initial Commit - Added Files to fail the first test case.

---
+++ RomanNumeralsKata.Tests/RomanNumeralsKataFromRomanNumericToNumberTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace RomanNumeralsKata.Tests;
+public class RomanNumeralsKataFromRomanNumericToNumberTests
+{ 
+
+    private RomanNumeralsKataFromRomanNumericsToNumber _RomanNumeralsKataFromRomanNumericsToNumber;
+
+    [SetUp]
+    public void Setup()
+    {
+        _RomanNumeralsKataFromRomanNumericsToNumber = new RomanNumeralsKataFromRomanNumericsToNumber();
+    }
+
+    [Test]
+    public void Get_Number_From_Roman_Numerics()
+    {
+        _RomanNumeralsKataFromRomanNumericsToNumber.GetNumberFromRomanNumerics("I").Should().Be(1);
+    }
+}
---
+++ RomanNumeralsKata/RomanNumeralsKataFromRomanNumericsToNumber.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumeralsKata
+{
+    public class RomanNumeralsKataFromRomanNumericsToNumber
+    {
+        public int GetNumberFromRomanNumerics(string roman)
+        {
+            return 0;
+        }
+    }
+}
